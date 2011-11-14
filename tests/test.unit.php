22127642a8ef4bf3204ea553f998590fe12c4587
Doug Avery <dougunderscorenelson@gmail.com>
2011-11-13 19:48:03 -0800

    First set up passing tests. Uh, am I doing this right?

--- tests/test.unit.php
+++ tests/test.unit.php
@@ -37,19 +37,67 @@ class Freeway_unit_test extends Testee_unit_test_case {
 	 $mock_object->assertNotEqual(true, false);
 
 	*/
+	function mock($array){
 	
-	function test_Freeway_ext()
-	{
-
 		Mock::generatePartial(
 			'Freeway_ext',
 			'MockFreeway_ext',
-			array('setup', 'should_execute')
+			$array
 		);
 
-		$mock_object = new MockFreeway_ext();
-		$mock_object->expectOnce('setup');
-		$mock_object->Freeway_ext();
+		return new MockFreeway_ext();
+
+	}
+	
+	function setup_Freeway_ext()
+	{
+	
+		$this->mock = $this->mock(array(
+			'setup', 
+			'should_execute', 
+			'load_routes', 
+			'remove_and_store_params', 
+			'uri_matches_pattern',
+			'store_original_uri',
+			'parse_new_uri_from_route',
+			'rebuild_uri_for_parsing',
+			'output_freeway_data'
+		));
+
+	}
+
+	function test_sets_up_but_doesnt_execute()
+	{
+
+		$this->setup_Freeway_ext();
+		$this->mock->returns('should_execute', false);
+		$this->mock->expectOnce('setup');
+		$this->mock->expectNever('store_original_uri');
+		$this->mock->expectNever('parse_new_uri_from_route');
+		$this->mock->Freeway_ext();
+
+	}
+
+	function test_sets_up_executes_doesnt_parse()
+	{
+
+		$this->setup_Freeway_ext();
+		$this->mock->returns('should_execute', true);
+		$this->mock->returns('uri_matches_pattern', false);
+		$this->mock->expectOnce('store_original_uri');
+		$this->mock->expectNever('parse_new_uri_from_route');
+		$this->mock->Freeway_ext();
+
+	}
+
+	function test_sets_up_executes_parses()
+	{
+
+		$this->setup_Freeway_ext();
+		$this->mock->returns('should_execute', true);
+		$this->mock->returns('uri_matches_pattern', true);
+		$this->mock->expectOnce('parse_new_uri_from_route');
+		$this->mock->Freeway_ext();
 
 	}
 
